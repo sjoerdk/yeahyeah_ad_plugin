dc37765c713f3e7b596d0d0bada007a7e74ddab0
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-12-18 17:03:33 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sjoerdk/yeahyeah_ad_plugin',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
